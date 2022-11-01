f4c28aa53501c5c5d505a9b89f2fcf6e05fdaec9
nisa0602 <nisahidayatika999@gmail.com>
2022-11-02 06:31:26 +0700

    state

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="grid grid-cols-3 items-center">
+      <div className="">
         <Food data={foods}/>
       </div>
     </div>
--- src/Food.jsx
+++ src/Food.jsx
@@ -3,23 +3,53 @@ import { Component } from "react";
 class Food extends Component{
     constructor(){
         super();
+        this.state = {
+            favFood: []
+        }
+    }
+    likeButton = (index) => {
+        const copyFavFood = [...this.state.favFood];
+        //mengambil index yg diklik, trs push ke copy-an state
+        copyFavFood.push(this.props.data[index]);
+        this.setState({
+            favFood: copyFavFood
+        })
     }
     render(){
         const foodList = this.props.data.map((food, i) => {
             return(
-                <div className="max-w-sm bg-white rounded-lg border border-gray-100 shadow-md dark:bg-white-600 dark:border-gray-300 mt-3">
+                <div key={i} className="max-w-sm bg-white rounded-lg border border-gray-100 shadow-md dark:bg-white-600 dark:border-gray-300 mt-3">
                     <img className="w-full px-8 pt-3" src={`${food.image}`} />
-                    <h5 key={i} className='text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black'>{food.name}</h5>
-                    <button type='button' className='text-white bg-blue-700 hover:bg-blue-800 focus:outline-none 
+                    <h5 className='text-center mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black'>{food.name}</h5>
+                    <button 
+                        onClick={()=>{this.likeButton(i)}}
+                        type='button' 
+                        className='ml-4 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none 
                             focus:ring-4 focus:ring-blue-300 font-medium rounded-full 
                             text-md px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 
-                            dark:hover:bg-blue-700 dark:focus:ring-blue-800'>Like this!</button>
+                            dark:hover:bg-blue-700 dark:focus:ring-blue-800'>
+                            Like this!
+                        </button>
+                </div>
+            )
+        })
+
+        const myFood = this.state.favFood.map((food, i) => {
+            return(
+                <div key={i}>
+                    <h1 className="">I like these foods:</h1>
+                    <h3>{food.name}</h3>
                 </div>
             )
         })
         return(
-                <>
-                    {foodList}
+                <>  
+                    <div className="grid grid-cols-3">
+                        {foodList}
+                    </div>
+                    <div>
+                        {myFood}
+                    </div>
                 </>
         )
     }
